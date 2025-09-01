2e01d05b9dc3ba977eb77c7d09e29e26c0729553
RamdanAG <ainimeeid@gmail.com>
2025-09-01 20:54:58 +0700

    Add About

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 export default function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero/>
-      <section className="z-9 min-h-screen bg-slate-500"/>
+      <About/>
     </main>
   )
 }
