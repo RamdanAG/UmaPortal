a8f697a72835c86069766ce93ce9c43f06fc786f
RamdanAG <ainimeeid@gmail.com>
2025-09-03 08:56:06 -0700

    Add BentoCard

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
-const
+
+const BentoCard = ({src,title,description}) =>{
+  return(
+    <div className='relative size-full'>
+      <video src={src} loop muted autoPlay className='absolute left-0 top-0 size-full object-cover object-center'/>
+      <div className='relative z-10 size-full flex-col justify-between p-5 text-blue-50'>
+        <div>
+          <h1 className='bento-title special-font'>{title}</h1>
+          {description && (
+            <p className='mt-3 max-w-64 text-xs md:text-base'>{description}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
 
 const Features = () => {
   return (
@@ -9,12 +25,20 @@ const Features = () => {
           <p className="font-circular-web text-lg text-blue-50">
             INTO THE METAGAME LAYER
           </p>
-        </div>
         <p className='max-w-md font-circular-web text-lg text-blue-50 opacity-50'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit omnis, eius commodi impedit magni et pariatur a in voluptatibus nihil!</p>
-      </div>
+        </div>
 
       <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
-        <BentoCard src="videos/feature-1.mp4" title={<>Radiant</>} description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, atque." isComingSoon={}/>
+        <BentoCard src="videos/feature-1.mp4" title={<>Radiant</>} description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, atque." />
+      </div>
+        <div className='grid h-[135vh] grid-cols-2 grid-rows-3 gap-7'>
+          <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+            <BentoCard src="videos/feature-2.mp4" title={<>Zigma</>} description="ldasnjdnadj djas dn ddajndajdnadjn"/>
+          </div>
+          <div>
+            
+          </div>
+        </div>
       </div>
     </section>
   )
