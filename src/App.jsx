b78414c8157769d54e335ef0b984d641799c5072
RamdanAG <ainimeeid@gmail.com>
2025-09-04 08:13:08 -0700

    Add About

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Features from "./components/Features";
 import Story from "./components/Story";
+import Contact from "./components/Contact";
 
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
       <About/>
       <Features/>
       <Story/>
+      <Contact/>
     </main>
   )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id='contact' className='my-20 min-h-96 w-screen px-10'>
+      <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
+        <div className='absolute -left-20 top- hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96'>
+            <div className='container-clip-path-1'>
+                <img src="img/contact-1.webp" alt="" />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
